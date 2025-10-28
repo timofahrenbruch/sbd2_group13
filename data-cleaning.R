7c555e79ea7542cbf3856282dc5906f28aabe3b7
Nicola Fricker <nicola.fricker@hotmail.com>
2025-10-28 22:54:24 +0100

    update: get cityname from mun_data

--- data-cleaning.R
+++ data-cleaning.R
@@ -25,11 +25,11 @@ data <- read_csv("DigitecLive_Final.csv")
 # read the official municipality data for later matching
 mun_data <- read_excel("bfsGdeNr.xlsx")
 
-
+######################## REPORT FOR DATA DIAGNOSIS ########################
 # create report for looking at the data diagnosis
 diagnose_web_report(data)
 
-
+######################## DUPLICATES ########################
 # check for duplicates
 sum(duplicated(data))     # how many?
 # delete the duplicates
@@ -37,14 +37,14 @@ data <- distinct(data)
 # verify that duplicates have been removed
 sum(duplicated(data))
 
-
+######################## MISSING VALUES ########################
 # visualize the missing values
 gg_miss_var(data) + labs(y = "Missing values per variable")
 # show the percentage of missing values per variable
 miss_var_summary(data) %>%
   arrange(desc(pct_miss))
 
-
+######################## CLEANING ########################
 # since salesPrice has 100% missing values, we will remove this column
 data <- data %>%
   select(-salesPrice)
@@ -55,27 +55,34 @@ data <- data %>%
 data <- data %>%
   select(-infos.Currency)
 
-
 # fill up missing slaesPrice.currency with "CHF" since everything is in swiss francs according to the specifications
 data$salesPrice.currency[is.na(data$salesPrice.currency)] <- "CHF"
 
+#### Cityname ####
+# count total rows where cityName does not match any of GDENAME in mun_data
+sum(!data$cityName %in% mun_data$GDENAME)
+
+# join mun_bfsnr ID with GDENR in mun_data and write GDENAME of it to cityname_clean
+data <- data %>%
+  left_join(mun_data, by = c("mun_bfsnr" = "GDENR")) %>%
+  mutate(cityName_clean = GDENAME) %>%
+  select(-GDENAME)
+
+# count total rows where cityName_clean match cityName
+sum(data$cityName == data$cityName_clean, na.rm = TRUE)
+
+# overwrite cityname with cityName_clean where mun_nhits == 1
+data <- data %>%
+  mutate(cityName = if_else(mun_nhits == 1, cityName_clean, cityName))
+
+# show how many rows where cityname and cityname_clean match after overwrite
+sum(data$cityName == data$cityName_clean, na.rm = TRUE)
+
+# NF 28.10: further cleaning needs to be done here
 
 # transform column info.Category (remove URL part)
 data$infos.Category <- str_remove(data$infos.Category, "^/de/s1/producttype/")
 
-# normalize city names for better matching
-normalize_names <- function(x) {
-  x <- tolower(x)                              # set to lowercase
-  x <- trimws(x)                               # remove heading an trailing white spaces
-  x <- stringi::stri_trans_general(x, "Latin-ASCII")  # remove umlauts and accents
-  x <- gsub("\\bst[\\.]?\\b", "sankt", x)      # St. → sankt
-  x <- gsub("\\bste[\\.]?\\b", "sainte", x)    # Ste. → sainte
-  x <- gsub("\\bsaint[\\.]?\\b", "sankt", x)   # Saint → sankt
-  x <- gsub("\\bsainte[\\.]?\\b", "sankt", x)  # Sainte → sankt
-  x <- gsub("[^a-z\\s]", "", x)                # remove everything but chars and whitespaces
-  x <- gsub("\\s+", " ", x)                    # remove double whitespaces
-  trimws(x)
-}
 
 # creating duplicated city name column for matching
 mun_data <- mun_data %>%
@@ -83,11 +90,7 @@ mun_data <- mun_data %>%
 
 # normalize city names in data for matching where nhits != 1
 data <- data %>%
-  mutate(cityName_clean = if_else(
-    mun_nhits != 1,
-    normalize_names(cityName),
-    cityName
-  ))
+  mutate(cityName_clean = if_else(mun_nhits != 1, normalize_names(cityName), cityName))
 
 # matching with a loop
 for (i in seq_len(nrow(data))) {
